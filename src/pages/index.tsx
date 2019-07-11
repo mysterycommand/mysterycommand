f21e8d94af6f568ed23828185d45be7472148d57
Matt Hayes <matt@mysterycommand.com>
2019-07-11 15:50:51 -0400

    feat: add three, and react-three-fiber

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Color, Vector3 } from 'three';
 
-const Index = () => <div />;
+const Index = () => (
+  <Canvas>
+    <group>
+      <line>
+        <geometry
+          attach="geometry"
+          vertices={[
+            [-1, 0, 0],
+            [0, 1, 0],
+            [1, 0, 0],
+            [0, -1, 0],
+            [-1, 0, 0],
+          ].map(v => new Vector3(...v))}
+        />
+        <lineBasicMaterial attach="material" color={new Color('black')} />
+      </line>
+      <mesh
+        onClick={e => console.log('click')}
+        onPointerOver={e => console.log('hover')}
+        onPointerOut={e => console.log('unhover')}
+      >
+        <octahedronGeometry attach="geometry" />
+        <meshBasicMaterial
+          attach="material"
+          color={new Color('peachpuff')}
+          opacity={0.5}
+          transparent
+        />
+      </mesh>
+    </group>
+  </Canvas>
+);
 
 export default Index;

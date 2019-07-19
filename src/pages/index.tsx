4aa9c9f00f405d3fb91d8eaa5478f8a0d5a04814
Matt Hayes <matt@mysterycommand.com>
2019-07-19 11:13:18 -0400

    chore: gatsby-browser and global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useRender } from 'react-three-fiber';
 import { Color, Vector3 } from 'three';
 
-const Index = () => (
-  <Canvas>
-    <group>
-      <line>
-        <geometry
-          attach="geometry"
-          vertices={[
-            [-1, 0, 0],
-            [0, 1, 0],
-            [1, 0, 0],
-            [0, -1, 0],
-            [-1, 0, 0],
-          ].map(v => new Vector3(...v))}
-        />
-        <lineBasicMaterial attach="material" color={new Color('black')} />
-      </line>
-      <mesh
-        onClick={e => console.log('click')}
-        onPointerOver={e => console.log('hover')}
-        onPointerOut={e => console.log('unhover')}
-      >
-        <octahedronGeometry attach="geometry" />
-        <meshBasicMaterial
-          attach="material"
-          color={new Color('peachpuff')}
-          opacity={0.5}
-          transparent
-        />
-      </mesh>
-    </group>
-  </Canvas>
-);
+const Index = () => {
+  useRender((args: any) => {
+    console.log(args);
+  });
+
+  return (
+    <Canvas>
+      <group>
+        <line>
+          <geometry
+            attach="geometry"
+            vertices={[
+              [-1, 0, 0],
+              [0, 1, 0],
+              [1, 0, 0],
+              [0, -1, 0],
+              [-1, 0, 0],
+            ].map(v => new Vector3(...v))}
+          />
+          <lineBasicMaterial attach="material" color={new Color('black')} />
+        </line>
+        <mesh
+          onClick={e => console.log('click')}
+          onPointerOver={e => console.log('hover')}
+          onPointerOut={e => console.log('unhover')}
+        >
+          <octahedronGeometry attach="geometry" />
+          <meshBasicMaterial
+            attach="material"
+            color={new Color('peachpuff')}
+            opacity={0.5}
+            transparent
+          />
+        </mesh>
+      </group>
+    </Canvas>
+  );
+};
 
 export default Index;

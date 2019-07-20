8fb9aa0ad8cf3a1a8350174fca75e93f88ec90ef
Matt Hayes <matt@mysterycommand.com>
2019-07-20 17:17:30 -0400

    feat(index): add a reducer, initial state, rehydration, and a simple updater

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,55 +1,59 @@
-import React, { FC } from 'react';
+import React, { FC, useReducer, useEffect } from 'react';
 import { FaGithub, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
-import { sample } from 'lodash';
+
 import './style.css';
 
-const App: FC = () => (
-  <div className="app">
-    <header>
-      <h1>Matt Hayes is</h1>
-      <h1>@mysterycommand</h1>
-      <ul>
-        <li>artist</li>
-        <li>
-          {sample([
-            'creative technologist',
-            'game maker',
-            'software engineer',
-            'web developer',
-          ])}
-        </li>
-        <li>
-          {sample([
-            'dabbler',
-            'dilettante',
-            'generalist',
-            'programmer',
-            'tinkerer',
-          ])}
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href="https://github.com/mysterycommand/" title="GitHub">
-            <FaGithub />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/mysterycommand/" title="Twitter">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/mysterycommand/"
-            title="Linkedin"
-          >
-            <FaLinkedinIn />
-          </a>
-        </li>
-      </ul>
-    </header>
-  </div>
-);
+import { initialState, reducer, ActionType } from '../../reducer';
+
+const App: FC = () => {
+  const [{ currentTitle, currentDescriptor }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      dispatch({ type: ActionType.Change });
+    }, 2500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [currentTitle, currentDescriptor]);
+
+  return (
+    <div className="app">
+      <header>
+        <h1>Matt Hayes is</h1>
+        <h1>@mysterycommand</h1>
+        <ul>
+          <li>artist</li>
+          <li>{currentTitle}</li>
+          <li>{currentDescriptor}</li>
+        </ul>
+        <ul>
+          <li>
+            <a href="https://github.com/mysterycommand/" title="GitHub">
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/mysterycommand/" title="Twitter">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/mysterycommand/"
+              title="Linkedin"
+            >
+              <FaLinkedinIn />
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+};
 
 export default App;

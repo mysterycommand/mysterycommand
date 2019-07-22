e76fd7eb2fcfa27c574a5ab5c119bde3295c2de8
Matt Hayes <matt@mysterycommand.com>
2019-07-22 14:56:47 -0400

    style(links): just some quick and dirty types on this little component

--- src/components/links/index.tsx
+++ src/components/links/index.tsx
@@ -1,51 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, AnchorHTMLAttributes } from 'react';
 import { FaGithub, FaTwitter, FaLinkedinIn, FaFilePdf } from 'react-icons/fa';
+import { IconType } from 'react-icons/lib/esm';
 
 import './style.css';
 
 import Hlist from '../hlist';
 
+const links: {
+  props: AnchorHTMLAttributes<HTMLAnchorElement>;
+  Icon: IconType;
+}[] = [
+  {
+    props: {
+      href: 'https://github.com/mysterycommand/',
+      title: 'GitHub',
+      rel: 'external',
+    },
+    Icon: FaGithub,
+  },
+  {
+    props: {
+      href: 'https://twitter.com/mysterycommand/',
+      title: 'Twitter',
+      rel: 'external',
+    },
+    Icon: FaTwitter,
+  },
+  {
+    props: {
+      href: 'https://www.linkedin.com/in/mysterycommand/',
+      title: 'Linkedin',
+      rel: 'external',
+    },
+    Icon: FaLinkedinIn,
+  },
+  {
+    props: {
+      href: `${process.env.PUBLIC_URL}/resume.pdf`,
+      title: 'My resume (PDF)',
+      download: 'Matt-Hayes-Resume.pdf',
+      type: 'application/pdf',
+      rel: 'author',
+    },
+    Icon: FaFilePdf,
+  },
+];
+
 const Links: FC = () => (
   <Hlist>
-    {[
-      {
-        htmlAnchorProps: {
-          href: 'https://github.com/mysterycommand/',
-          title: 'GitHub',
-          rel: 'external',
-        },
-        Icon: FaGithub,
-      },
-      {
-        htmlAnchorProps: {
-          href: 'https://twitter.com/mysterycommand/',
-          title: 'Twitter',
-          rel: 'external',
-        },
-        Icon: FaTwitter,
-      },
-      {
-        htmlAnchorProps: {
-          href: 'https://www.linkedin.com/in/mysterycommand/',
-          title: 'Linkedin',
-          rel: 'external',
-        },
-        Icon: FaLinkedinIn,
-      },
-      {
-        htmlAnchorProps: {
-          href: `${process.env.PUBLIC_URL}/resume.pdf`,
-          title: 'My resume (PDF)',
-          download: 'Matt-Hayes-Resume.pdf',
-          type: 'application/pdf',
-          rel: 'author',
-        },
-        Icon: FaFilePdf,
-      },
-    ].map(({ htmlAnchorProps, Icon }) => {
-      const { title, href } = htmlAnchorProps;
+    {links.map(({ props, Icon }) => {
+      const { title, href } = props;
       return (
-        <a key={`${title}: ${href}`} {...htmlAnchorProps}>
+        <a key={`${title}: ${href}`} {...props}>
           <Icon />
         </a>
       );

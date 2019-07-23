43ba3a5e44c12e8105f300b6d6fd24ad45e2950e
Matt Hayes <matt@mysterycommand.com>
2019-07-23 12:23:20 -0400

    feat(app): add link to self in header

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -9,7 +9,9 @@ const App: FC = () => (
   <div className="app">
     <header>
       <h1>Matt Hayes is</h1>
-      <h1>@mysterycommand</h1>
+      <h1>
+        <a href={process.env.PUBLIC_URL}>@mysterycommand</a>
+      </h1>
       <Descs />
       <Links />
     </header>

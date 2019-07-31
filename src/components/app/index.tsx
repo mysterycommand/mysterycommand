7f2b306baa91e8a22bb85e0729c772b9c64b64e2
Matt Hayes <matt@mysterycommand.com>
2019-07-31 10:35:17 -0400

    refactor(components/app): add titles and descriptors to the reducer
    going to add the full list to the head via react-helmet so there's one source of truth

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,23 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useReducer } from 'react';
 
 import './style.css';
 
+import { initialState, reducer } from '../../reducer';
 import Descs from '../descs';
 import Links from '../links';
 import Splash from '../splash';
 
-const App: FC = () => (
-  <div className="app">
-    <Splash />
-    <header>
-      <h1>Matt Hayes is</h1>
-      <h1>
-        <a href={process.env.PUBLIC_URL}>@mysterycommand</a>
-      </h1>
-      <Descs />
-      <Links />
-    </header>
-  </div>
-);
+const App: FC = () => {
+  const [{ titles, descriptors }] = useReducer(reducer, initialState);
+  console.log(titles, descriptors);
+
+  return (
+    <div className="app">
+      <Splash />
+      <header>
+        <h1>Matt Hayes is</h1>
+        <h1>
+          <a href={process.env.PUBLIC_URL}>@mysterycommand</a>
+        </h1>
+        <Descs />
+        <Links />
+      </header>
+    </div>
+  );
+};
 
 export default App;
--- src/reducer.ts
+++ src/reducer.ts
@@ -18,31 +18,35 @@ const descriptors = [
   'tinkerer',
 ];
 
+type State = {
+  titles: string[];
+  currentTitle: string;
+  descriptors: string[];
+  currentDescriptor: string;
+};
+
+export enum ActionType {
+  Change,
+}
+
+type Action = {
+  type: ActionType;
+};
+
 // @ts-ignore: 2339
 const state = window.__STATE__ as State;
 // @ts-ignore: 2339
 delete window.__STATE__;
 
 export const initialState: State = state || {
+  titles,
   currentTitle: sample(titles)!, // 'computer programmer', //
+  descriptors,
   currentDescriptor: sample(descriptors)!, // 'philosopher', //
 };
 
 // @ts-ignore: 2339
 window.snapSaveState = () => ({ __STATE__: initialState });
-
-export enum ActionType {
-  Change,
-}
-
-type State = {
-  currentTitle: string;
-  currentDescriptor: string;
-};
-
-type Action = {
-  type: ActionType;
-};
 
 function getFresh(current: string, pool: string[]): string {
   let fresh = sample(pool)!;
@@ -58,6 +62,8 @@ export function reducer(state: State, action: Action): State {
   switch (action.type) {
     case ActionType.Change:
       return {
+        titles,
+        descriptors,
         currentTitle: getFresh(state.currentTitle, titles),
         currentDescriptor: getFresh(state.currentDescriptor, descriptors),
       };

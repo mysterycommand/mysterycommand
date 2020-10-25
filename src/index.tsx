ae29d6eabcf4b277ccc74b479ead7c577ba58687
Matt Hayes <matt@mysterycommand.com>
2020-10-25 17:33:26 -0400

    fix: remove service-worker and import (wasn't actually using it anyway)

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import { hydrate, render } from 'react-dom';
 
 import './style.css';
 import App from './components/app';
-import * as sw from './service-worker';
 
 const rootEl = document.getElementById('root');
 (rootEl && rootEl.hasChildNodes() ? hydrate : render)(<App />, rootEl);
-
-sw.unregister();
